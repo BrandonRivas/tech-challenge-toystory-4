abb753078e20cda770ffd8283f0e94fe004122d5
Brandon Rivas <89816116+BrandonRivas@users.noreply.github.com>
2022-07-20 22:41:45 -0700

    Finished Project This contains stylised page, finished css, and finished javascript

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 console.log("To infinity and beyond.");
 
 // write your JavaScript here
+var post = document.getElementById("post");
+post.addEventListener("click", function () {
+  var commentBoxValue = document.getElementById("comment-box").value;
+
+  var li = document.createElement("li");
+  var text = document.createTextNode(commentBoxValue);
+  li.appendChild(text);
+  document.getElementById("unordered").appendChild(li);
+});
